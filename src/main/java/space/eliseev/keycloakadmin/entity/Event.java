eb1e9d99ea8e92f12709f12b7cf752eec767ddaf
Alexander <105540044+sotnicoff@users.noreply.github.com>
2022-07-15 16:40:27 +0300

    Update
    code styling

--- src/main/java/space/eliseev/keycloakadmin/entity/Event.java
+++ src/main/java/space/eliseev/keycloakadmin/entity/Event.java
@@ -12,22 +12,31 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "event_entity")
 public class Event extends BaseEntity {
+    
     @Column(name = "client_id")
     private String clientId;
+    
     @Column(name = "realm_id")
     private String realmId;
+    
     @Column(name = "session_id")
     private String sessionId;
+    
     @Column(name = "user_id")
     private String userId;
+    
     @Column(name = "details_json")
     private String detailsJson;
+    
     @Column(name = "error")
     private String error;
+    
     @Column(name = "ip_address")
     private String ipAddress;
+    
     @Column(name = "event_time")
     private long eventTime;
+    
     @Column(name = "type")
     private String type;
 }
