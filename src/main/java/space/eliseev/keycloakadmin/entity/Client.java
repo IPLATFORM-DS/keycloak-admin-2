c25f34d992a517eb95ae7c89304b37abc2573177
youRock <yyyuras191@mail.ru>
2022-07-15 17:00:14 +0300

    Создан entity-класс Client на основании таблицы client

--- src/main/java/space/eliseev/keycloakadmin/entity/Client.java
+++ src/main/java/space/eliseev/keycloakadmin/entity/Client.java
@@ -1,6 +1,7 @@
 package space.eliseev.keycloakadmin.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -10,6 +11,7 @@ import javax.persistence.Table;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 @Table (name = "client_entity")
 public class Client extends BaseEntity {
 
