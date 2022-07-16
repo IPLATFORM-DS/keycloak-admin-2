bc77b068c490cfc93697b38c844309d9e1ac82db
annalana <devil-villow@inbox.ru>
2022-07-16 19:43:54 +0300

    KA-12 Исправлены контроллеры. Возвращают ResponseEntity, методы публичные. Из RoleRepository удалены лишние методы. Названия методов в сервисах изменены для обеспечения универсальности названий (по примеру UserService).

--- src/main/java/space/eliseev/keycloakadmin/service/RoleService.java
+++ src/main/java/space/eliseev/keycloakadmin/service/RoleService.java
@@ -1,15 +1,18 @@
 package space.eliseev.keycloakadmin.service;
 
+import lombok.NonNull;
 import space.eliseev.keycloakadmin.entity.Role;
 
 import java.util.List;
 import java.util.Optional;
 
 /**
-    * Получение информации о ролях
+ * Получение информации о ролях
  */
 public interface RoleService {
-    List<Role> findAll();
-    Optional<Role> findById(String s);
-    List<Role> findByName(String name);
+    List<Role> getAllRoles();
+
+    Optional<Role> getById(@NonNull String id);
+
+    List<Role> getByName(@NonNull String name);
 }
--- src/main/java/space/eliseev/keycloakadmin/service/RoleServiceImpl.java
+++ src/main/java/space/eliseev/keycloakadmin/service/RoleServiceImpl.java
@@ -1,5 +1,6 @@
 package space.eliseev.keycloakadmin.service;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import space.eliseev.keycloakadmin.entity.Role;
@@ -16,19 +17,36 @@ import java.util.Optional;
 public class RoleServiceImpl implements RoleService {
     private final RoleRepository roleRepository;
 
+    /**
+     * Получить список всех ролей
+     *
+     * @return список всех ролей
+     */
     @Override
-    public List<Role> findAll() {
+    public List<Role> getAllRoles() {
         return roleRepository.findAll();
     }
 
+    /**
+     * Получить объект роль по идентификатору
+     *
+     * @param id Идентификатор роли
+     * @return роль (или пустой Optional)
+     */
     @Override
-    public Optional<Role> findById(String id) {
+    public Optional<Role> getById(@NonNull String id) {
         return roleRepository.findById(id);
     }
 
+    /**
+     * Получить роль по имени (может быть список из нескольких ролей при совпадении названий
+     * для разных Realm.
+     *
+     * @param name - название роли (поле name)
+     * @return список ролей (названия роли могут совпадать в разных Realm)
+     */
     @Override
-    public List<Role> findByName(String name) {
+    public List<Role> getByName(@NonNull String name) {
         return roleRepository.findByName(name);
     }
-
 }
