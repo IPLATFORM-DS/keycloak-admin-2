5f7e17c208dbd5c227107c37074d7e4fbb319c52
Alexander <olek.sotnic@gmail.com>
2022-07-20 10:13:46 +0300

    Реализация UserDTO

---
+++ src/main/java/space/eliseev/keycloakadmin/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package space.eliseev.keycloakadmin.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+    private String email;
+    private Boolean emailVerified;
+    private String firstName;
+    private String lastName;
+    // Соответствует текстовому полю realm_id в таблице realm
+    private String realmName;
+    private String username;
+    private Long createdTimestamp;
+}
