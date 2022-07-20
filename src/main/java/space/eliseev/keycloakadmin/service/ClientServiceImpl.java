e40d6f5142b0669d45b601495c08f478cf219eeb
youRock <yyyuras191@mail.ru>
2022-07-20 20:08:24 +0300

    Реализация clientDto (убрал лишнее)

--- src/main/java/space/eliseev/keycloakadmin/service/ClientServiceImpl.java
+++ src/main/java/space/eliseev/keycloakadmin/service/ClientServiceImpl.java
@@ -3,21 +3,17 @@ package space.eliseev.keycloakadmin.service;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import space.eliseev.keycloakadmin.dto.ClientDto;
-import space.eliseev.keycloakadmin.dto.ClientDtoMapper;
 import space.eliseev.keycloakadmin.entity.Client;
 import space.eliseev.keycloakadmin.repository.ClientRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ClientServiceImpl implements ClientService {
 
     private final ClientRepository clientRepository;
-    private final ClientDtoMapper clientDtoMapper;
 
     @Override
     public List<Client> getAllClients() {
@@ -34,19 +30,5 @@ public class ClientServiceImpl implements ClientService {
         return clientRepository.findClientByName(name);
     }
 
-    public List<ClientDto> getAllClientsDto() {
-        return getAllClients().stream().map(clientDtoMapper :: mapToClientDto)
-                .collect(Collectors.toList());
-    }
-
-    public Optional<ClientDto> getClientDtoById(@NonNull final String id) {
-        Client client = getById(id).orElse(new Client());
-        return Optional.ofNullable(clientDtoMapper.mapToClientDto(client));
-    }
-
-    public Optional<ClientDto> getClientDtoByName(@NonNull final String name) {
-        Client client = getClientByName(name).orElse(new Client());
-        return Optional.ofNullable(clientDtoMapper.mapToClientDto(client));
-    }
 
 }
