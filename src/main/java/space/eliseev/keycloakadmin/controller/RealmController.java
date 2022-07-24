afcb29c81011a34f9bdd73d51baf36748824f18b
RalfMog <yurispb.mogilevich@gmail.com>
2022-07-24 22:45:48 +0500

    KA-39 описание RealmController

--- src/main/java/space/eliseev/keycloakadmin/controller/RealmController.java
+++ src/main/java/space/eliseev/keycloakadmin/controller/RealmController.java
@@ -31,7 +31,7 @@ public class RealmController {
 
     private final RealmService realmService;
 
-    @Operation(summary = "Get all realms", description = "It can be used to get list of all realms in all realms",
+    @Operation(summary = "Get all realms", description = "It can be used to get list of all realms",
             tags = {"realm"})
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
@@ -57,12 +57,11 @@ public class RealmController {
                 .orElseGet(() -> new ResponseEntity<>(new Realm(), HttpStatus.NOT_FOUND));
     }
 
-    @Operation(summary = "Get realm by name", description = "It returns list of realms with specified name in all realms",
-            tags = {"realm"})
+    @Operation(summary = "Get realm by name", description = "It returns one realm with specified name", tags = {"realm"})
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
-                    array = @ArraySchema(schema = @Schema(implementation = Realm.class))),
-                    description = "successful operation (may be empty list)")
+            @ApiResponse(responseCode = "200", description = "successful operation",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Realm.class))),
+            @ApiResponse(responseCode = "404", description = "realm not found", content = @Content)
     })
     @GetMapping(value = "/getByName/{name}", produces = {"application/json"})
     public ResponseEntity<Realm> getRealmByName(@PathVariable(name = "name") String name) {
