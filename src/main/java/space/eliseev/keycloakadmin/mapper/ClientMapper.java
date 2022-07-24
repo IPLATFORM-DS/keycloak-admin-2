8cb507f9ae7333c80e356f37f98708100a6672f1
Yuriy Ryabkov <yyyuras191@mail.ru>
2022-07-24 17:02:47 +0300

    Описание ClientController (создал отдельный пакет mapper, добавил зависимость spring)

--- src/main/java/space/eliseev/keycloakadmin/mapper/ClientMapper.java
+++ src/main/java/space/eliseev/keycloakadmin/mapper/ClientMapper.java
@@ -5,7 +5,7 @@ import org.mapstruct.Mapping;
 import space.eliseev.keycloakadmin.dto.ClientDto;
 import space.eliseev.keycloakadmin.entity.Client;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface ClientMapper {
     @Mapping(target = "realmName", source = "realmId")
     ClientDto clientToClientDto(Client client);
