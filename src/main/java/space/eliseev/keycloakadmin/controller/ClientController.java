8031a485cbd6c962a8875751f26a8d1b696db6dc
youRock19 <97917548+youRock19@users.noreply.github.com>
2022-07-26 18:24:03 +0300

    Описание ClientController (#28)

--- src/main/java/space/eliseev/keycloakadmin/controller/ClientController.java
+++ src/main/java/space/eliseev/keycloakadmin/controller/ClientController.java
@@ -1,6 +1,13 @@
 package space.eliseev.keycloakadmin.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,15 +24,29 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/client", produces = "application/json; charset=UTF-8")
+@Tag(name = "client", description = "The Client API")
 public class ClientController {
 
     private final ClientServiceImpl clientService;
-
+    @Operation(summary = "Get all clients", description = "It can be used to get list of all clients in all realms")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = ClientDto.class))),
+                    description = "Successful operation (List may be empty)")
+    })
     @GetMapping(value = "/getAll")
     ResponseEntity<List<ClientDto>> getClients() {
         return new ResponseEntity<>(clientService.getAllClients(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get client by ID", description = "It returns one client with specified id",
+            tags = {"client"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ClientDto.class)),
+                    description = "Successful operation"),
+            @ApiResponse(responseCode = "404", content = @Content, description = "User not found")
+    })
     @GetMapping(value = "/get/{id}")
     ResponseEntity<ClientDto> getClientById(@PathVariable("id") String id) {
         final Optional<ClientDto> client = clientService.getById(id);
@@ -34,7 +55,14 @@ public class ClientController {
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-
+    @Operation(summary = "Get client by name", description = "It returns one client with specified name",
+            tags = {"client"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ClientDto.class)),
+                    description = "Successful operation"),
+            @ApiResponse(responseCode = "404", content = @Content, description = "User not found")
+    })
     @GetMapping(value = "/getByName/{name}")
     ResponseEntity<ClientDto> getClientByName(@PathVariable("name") String name) {
         final Optional<ClientDto> client = clientService.getClientByName(name);
