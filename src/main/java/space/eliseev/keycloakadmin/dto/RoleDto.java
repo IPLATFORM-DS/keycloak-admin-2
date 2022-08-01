431e8376179ee34fe3213a59faf8fa8845946c8c
RalfMog <yurispb.mogilevich@gmail.com>
2022-08-01 12:59:46 +0500

    KA-55 добавление печатных форм для Role

--- src/main/java/space/eliseev/keycloakadmin/dto/RoleDto.java
+++ src/main/java/space/eliseev/keycloakadmin/dto/RoleDto.java
@@ -5,10 +5,14 @@ import lombok.Data;
 @Data
 public class RoleDto {
     private String name;
+
     private Boolean clientRole;
+
     private String description;
+
     // Соответствует текстовому полю client_id в таблице client
     private String clientName;
+
     // Соответствует текстовому полю name в таблице realm
     private String realmName;
 }
