a2f4e42e46ab258cfdf102a8ede46b639aa1d650
QayTem <96597936+QayTem@users.noreply.github.com>
2022-08-12 15:34:30 +0500

    Добавить в UserMapper маппинг realmName и дату (#44)

--- src/main/java/space/eliseev/keycloakadmin/mapper/UserMapper.java
+++ src/main/java/space/eliseev/keycloakadmin/mapper/UserMapper.java
@@ -1,10 +1,14 @@
 package space.eliseev.keycloakadmin.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import space.eliseev.keycloakadmin.commons.TimeUtils;
 import space.eliseev.keycloakadmin.dto.UserDto;
 import space.eliseev.keycloakadmin.entity.User;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = TimeUtils.class)
 public interface UserMapper {
+    @Mapping(target = "realmName", source = "realm.name")
+    @Mapping(target = "createdTimestampLocalDateTime", source = "createdTimestamp")
     UserDto userToUserDto(User user);
 }
